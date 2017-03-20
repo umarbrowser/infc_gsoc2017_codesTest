5ce5901d3279441cfba553930c1c0bcb36b3dacb
pyc0d3r <umarbrowser20@gmail.com>
2017-03-20 09:44:56 +0100

    add some codes
    i just added some codes

--- psycho1.py
+++ psycho1.py
@@ -1,22 +1,29 @@
+#!/usr/bin/ python
+# imports
 from psychopy import visual, core
 
+#create the window with 400X400
 win = visual.Window([400,400])
+#crete the message on the screen Hello from:
 message = visual.TextStim(win, text='Hello from :')
+#draw
 message.setAutoDraw(True)
+#window will flip
 win.flip()
+#wait for 2 seconds
 core.wait(2.0)
-message.setText('umar')
+message.setText('incf in psychopy')
 win.flip()
-core.wait(2.0)
+core.wait(4.0)
 message.setText('known as:')
 win.flip()
 core.wait(2.0)
-message.setText('pyc0d3r')
+message.setText('The International Neuroinformatics Coordinating Facility (INCF)')
+win.flip()
+core.wait(7.0)
+message.setText('there website')
 win.flip()
 core.wait(2.0)
-message.setText('His website')
+message.setText('http://incf.org/')
 win.flip()
-core.wait(2.0)
-message.setText('http://www.umarbrowser.co.vu/')
-win.flip()
-core.wait(2.0)
+core.wait(9.0)
