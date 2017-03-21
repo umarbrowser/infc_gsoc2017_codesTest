51e281093935cd7a752c639d01e2c50f346969b8
pyc0d3r <umarbrowser20@gmail.com>
2017-03-21 11:56:30 +0100

    added some words
    i just commit the code.

--- lots_button.py
+++ lots_button.py
@@ -1,13 +1,19 @@
+# import the gtk library
 import gtk
 
+# create the application or class
 class PyApp(gtk.Window):
 
     def __init__(self):
         super(PyApp, self).__init__()
-
-        self.set_title('janyo kasa')
+        
+        # set the titile name
+        self.set_title('incf_buttons_test')
+        # set the size of the window 400 x 300
         self.set_size_request(400, 300)
+        # set the position with is the window center
         self.set_position(gtk.WIN_POS_CENTER)
+        # starting the actual codes
         sw = gtk.ScrolledWindow()
         table = gtk.Table(10, 10)
         table.set_row_spacings(10)
@@ -19,7 +25,7 @@ class PyApp(gtk.Window):
                 table.attach(btn, i, i+1, j, j+1)
         sw.add_with_viewport(table)
         self.add(sw)
-
+        # finished the program by clicking quit button
         self.connect('destroy', gtk.main_quit)
         self.show_all()
 
